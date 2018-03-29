a3b524db54d04a62133fab8e0dcf0afce2ae2b5f
Dilshat Aliev <daliev@optimal-dynamics.com>
2018-03-29 14:08:35 +0600

    temp workaround of zfs not found issue

--- lib/fs/zfs.go
+++ lib/fs/zfs.go
@@ -7,23 +7,23 @@ import (
 )
 
 func IsDatasetReadOnly(dataset string) bool {
-	out, err := exec.Command("/bin/bash", "-c", "/sbin/zfs get readonly -H "+dataset+" | awk '{print $3}' ").CombinedOutput()
+	out, err := exec.Command("/bin/bash", "-c", "/var/lib/snapd/hostfs/sbin/zfs get readonly -H "+dataset+" | awk '{print $3}' ").CombinedOutput()
 	output := strings.TrimSpace(string(out))
 	log.Check(log.FatalLevel, "Getting zfs readonly property "+output, err)
 	return output == "on"
 }
 
 func RemoveDataset(dataset string) {
-	out, err := exec.Command("/bin/bash", "-c", "/sbin/zfs destroy -r "+dataset).CombinedOutput()
+	out, err := exec.Command("/bin/bash", "-c", "/var/lib/snapd/hostfs/sbin/zfs destroy -r "+dataset).CombinedOutput()
 	log.Check(log.ErrorLevel, "Removing zfs dataset "+string(out), err)
 }
 
 func CreateDataset(dataset string) {
-	out, err := exec.Command("/bin/bash", "-c", "/sbin/zfs create "+dataset).CombinedOutput()
+	out, err := exec.Command("/bin/bash", "-c", "/var/lib/snapd/hostfs/sbin/zfs create "+dataset).CombinedOutput()
 	log.Check(log.ErrorLevel, "Creating zfs dataset "+string(out), err)
 }
 
 func ReceiveStream(delta string, destDataset string) {
-	out, err := exec.Command("/bin/bash", "-c", "cat "+delta+" | /sbin/zfs receive "+destDataset).CombinedOutput()
+	out, err := exec.Command("/bin/bash", "-c", "cat "+delta+" | /var/lib/snapd/hostfs/sbin/zfs receive "+destDataset).CombinedOutput()
 	log.Check(log.ErrorLevel, "Creating zfs stream "+string(out), err)
 }
