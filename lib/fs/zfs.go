8e08d528f96346e43b3fe6fadbc3eeb54e4be551
Dilshat Aliev <daliev@optimal-dynamics.com>
2018-03-30 09:53:34 +0600

    added paths to logs

--- lib/fs/zfs.go
+++ lib/fs/zfs.go
@@ -20,20 +20,20 @@ func DatasetExists(dataset string) bool {
 
 func SetDatasetReadOnly(dataset string) {
 	out, err := exec.Command("/bin/bash", "-c", "zfs set readonly=on "+dataset).CombinedOutput()
-	log.Check(log.ErrorLevel, "Setting zfs readonly property "+string(out), err)
+	log.Check(log.ErrorLevel, "Setting zfs dataset readonly "+dataset+"\n"+string(out), err)
 }
 
 func RemoveDataset(dataset string) {
 	out, err := exec.Command("/bin/bash", "-c", "zfs destroy -r "+dataset).CombinedOutput()
-	log.Check(log.ErrorLevel, "Removing zfs dataset "+string(out), err)
+	log.Check(log.ErrorLevel, "Removing zfs dataset "+dataset+"\n"+string(out), err)
 }
 
 func CreateDataset(dataset string) {
 	out, err := exec.Command("/bin/bash", "-c", "zfs create "+dataset).CombinedOutput()
-	log.Check(log.ErrorLevel, "Creating zfs dataset "+string(out), err)
+	log.Check(log.ErrorLevel, "Creating zfs dataset "+dataset+"\n"+string(out), err)
 }
 
 func ReceiveStream(delta string, destDataset string) {
 	out, err := exec.Command("/bin/bash", "-c", "zfs receive "+destDataset+" < "+delta).CombinedOutput()
-	log.Check(log.ErrorLevel, "Creating zfs stream "+string(out), err)
+	log.Check(log.ErrorLevel, "Creating zfs stream "+destDataset+" < "+delta+"\n"+string(out), err)
 }
